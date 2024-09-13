4af7fe270dd323bbf68513874dc31f5a49884add
VordaNick <vordanick@gmail.com>
2024-09-13 20:35:29 +0100

    Rename future_age_calculator.py to python_introduction/future_age_calculator.py

--- future_age_calculator.py
+++
@@ -1,2 +0,0 @@
-age=int(input("How old are you?"))
-print(f"In 2050, you will be {age+27} years old.")
---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,2 @@
+age=int(input("How old are you?"))
+print(f"In 2050, you will be {age+27} years old.")
