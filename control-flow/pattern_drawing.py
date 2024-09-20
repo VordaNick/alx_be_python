65092a987ba01cb98dacd3c4004e7da9d6931831
VordaNick <vordanick@gmail.com>
2024-09-20 14:43:57 +0100

    Corrected

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,3 +1,8 @@
-n = int(input("Enter the size of the pattern: "))
+while True:
+    n = int(input("Enter the size of the pattern: "))
+    if n > 0:
+        break
+    else:
+        print("Invalid input. Please enter a positive integer.")
 for i in range(n):
     print("*" * n)
