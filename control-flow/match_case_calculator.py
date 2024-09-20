999786ed25e14686b221e403834a8844e24e1abc
VordaNick <vordanick@gmail.com>
2024-09-20 14:01:08 +0100

    Corrected Second Number

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,5 +1,5 @@
 num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number"))
+num2 = int(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /): ")
 match operation:
     case "+" :
