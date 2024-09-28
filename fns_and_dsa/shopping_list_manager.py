4a0eb158cb4d454f5bb3b57d938858b719fbe861
VordaNick <vordanick@gmail.com>
2024-09-28 13:46:33 +0100

    Added

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,24 @@
+def display_menu():
+    print('Shopping List Manager')
+    print('1. Add Item')
+    print('2. Remove Item')
+    print('3. View List')
+    print('4. Exit')
+def main():
+    shopping_list = []
+    while True:
+        display_menu
+        choice = input('Enter your choice: ')
+        if choice == '1':
+            item = input('What item will you like to add?: ')
+            shopping_list.append({item})
+        elif choice == '2':
+            item = input('Which item would you like to remove?: ')
+            shopping_list.remove({item})
+        elif choice == '3':
+            print(shopping_list)
+        elif choice == '4':
+            print('Goodbye!')
+            break
+        else:
+            print('Invalid choice. Please try again.')
