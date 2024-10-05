100e2c452ac81534d95cb98ac5f589fe16db3b67
VordaNick <vordanick@gmail.com>
2024-10-05 15:34:58 +0100

    Correct

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -6,12 +6,11 @@ class BankAccount:
         
     def withdraw(self, amount):
         can_withdraw = 0 < amount <= self.balance
-        while True:
-            if can_withdraw:
-                self.balance -= amount
-            else:
-                print("Insufficient Funds")
-                break
+        if can_withdraw:
+            self.balance -= amount
+            return True
+        else:
+            return False
     def display_balance(self):
         print(f'Current Balance: ${self.balance}')
         
