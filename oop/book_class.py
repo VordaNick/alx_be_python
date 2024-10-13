096070588e4431f57eb3625334253c4286945a39
VordaNick <vordanick@gmail.com>
2024-10-13 20:31:17 +0100

    Add files via upload

--- oop/book_class.py
+++ oop/book_class.py
@@ -11,4 +11,4 @@ class Book:
         return f"Book('{self.title}', '{self.author}', {self.year})"
     
     def __del__(self):
-        print(f"Deleting {self.title}"
+        print(f"Deleting {self.title}")
